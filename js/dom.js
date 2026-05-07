dab9b74caa2ac7f056d196ca28a814a2467224a8
sergiodesenho1-collab <sergiodesenho1@gmail.com>
2026-05-07 11:47:47 -0300

    adicionando obter tarefa,limpar o input e adicionar tarefa na lista

--- js/dom.js
+++ js/dom.js
@@ -1,1 +1,18 @@
 //dom.js
+export function obterTextoTarefa() {
+  const input = document.querySelector("#input-tarefa");
+  return input.value;
+}
+
+export function limparInput() {
+  document.querySelector("#input-tarefa").value = "";
+}
+
+export function adicionarTarefaNaLista(texto) {
+  const lista = document.querySelector("#lista-tarefas");
+
+  const li = document.createElement("li");
+  li.textContent = texto;
+
+  lista.appendChild(li);
+}
--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-console.log("Aplicação Gerenciador de tarefas");
+import {
+  obterTextoTarefa,
+  limparInput,
+  adicionarTarefaNaLista,
+} from "./dom.js";
+
+const form = document.querySelector("#form-tarefa");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const texto = obterTextoTarefa();
+
+  adicionarTarefaNaLista(texto);
+  limparInput();
+});
